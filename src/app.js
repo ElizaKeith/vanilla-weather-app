f7e6403c3362515226ec2914c5c490536704debf
ek0179 <Liz.keith56@gmail.com>
2023-10-24 00:19:50 -0400

    added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -17,12 +17,16 @@ let description= document.querySelector("#description");
 let humidity= document.querySelector("#humidity");
 let windElement= document.querySelector("#wind");
 let dateElement= document.querySelector("#date");
+let iconElement= document.querySelector("#icon");
+
 temperatureElement.innerHTML=Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
 description.innerHTML=response.data.weather[0].description;
 humidity.innerHTML=response.data.main.humidity;
 windElement.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatDate(response.data.dt*1000)
+iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+iconElement.setAttribute("alt", response.data.weather[0].description)
 }
 
 
